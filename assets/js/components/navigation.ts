bf1c4c47f3e191b55c77b4beda8fb6fedb4161ec
Barish Barla <barish.barla10@gmail.com>
2022-07-10 23:56:06 +0530

    Make mobile nav more user friendly

--- assets/js/components/navigation.ts
+++ assets/js/components/navigation.ts
@@ -2,14 +2,39 @@ console.log("from navigation.ts");
 
 const mobNav = document.querySelector(".mobnav") as HTMLDivElement;
 const mobNavToggle = document.querySelector(".mobnav__toggle") as HTMLDivElement;
+const content = document.querySelector("#content") as HTMLDivElement;
+
+const overlayClass : string = "mobnav--overlay";
 
 mobNavToggle.addEventListener("click", () => {
   const visibility = mobNav.getAttribute("data-visible");
   if (visibility === "false") {
     mobNav.setAttribute("data-visible", "true");
+    content.classList.add(overlayClass);
   } else {
     mobNav.setAttribute("data-visible", "false");
+    content.classList.remove(overlayClass)
   }
 });
 
-// TODO: Detect any scroll if scroll is detected hide the mobile nav
+// To detect click out side of the slide menu
+
+content.addEventListener("click", (event: Event) => {
+  event.stopPropagation()
+  const visibility = mobNav.getAttribute("data-visible");
+  if (visibility === "true") {
+    mobNav.setAttribute("data-visible", "false");
+    content.classList.remove(overlayClass)
+  }
+})
+
+// Detect any scroll if scroll is detected hide the mobile nav
+
+window.addEventListener("scroll", (event: Event) => {
+  event.stopPropagation()
+  const visibility = mobNav.getAttribute("data-visible");
+  if (visibility === "true") {
+    mobNav.setAttribute("data-visible", "false");
+    content.classList.remove(overlayClass)
+  }
+})
